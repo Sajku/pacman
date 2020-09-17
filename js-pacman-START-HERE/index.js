bd9f2ed4c1caa8cd2334b3327cd54849e0bb11b8
Sajku <kielek0412@gmail.com>
2020-09-17 18:48:03 +0200

    Some progress

---
+++ js-pacman-START-HERE/index.js
@@ -0,0 +1,33 @@
+import {LEVEL, OBJECT_TYPE} from "./setup";
+
+// DOM Elements
+const gameGrid = document.querySelector("#game");
+const scoreTable = document.querySelector("#score");
+const startbutton = document.querySelector("#start-button");
+
+// Game Constants
+const POWER_PILL_TIME = 10000; // ms
+const GLOBAL_SPEED = 80; // ms
+
+// Initial Setup
+let score = 0;
+let timer = null;
+let gameWin = false;
+let powerPillActive = false;
+let powerPillTimer = null;
+
+function gameOver(pacman, grid) {
+    
+}
+
+function checkCollision(pacman, ghosts) {
+
+}
+
+function gameLoop(pacman, ghosts) {
+
+}
+
+function startGame() {
+
+}
